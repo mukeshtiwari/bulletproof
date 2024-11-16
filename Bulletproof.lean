30e58d8227f78cd254075fb20a5cc1f7e9ff9c91
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2024-11-16 09:53:23 +0000

    First commit for Bulletproof

---
+++ Bulletproof.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Bulletproof` library.
+-- Import modules here that should be built as part of the library.
+import Bulletproof.Basic
---
+++ Bulletproof/Basic.lean
@@ -0,0 +1,8 @@
+/- some mathlib, batteries, aesop imports -/
+
+/- here goes definition of bulletproof -/
+section definition
+
+  def fn (n : Nat) : Nat := n + 1
+
+end definition
