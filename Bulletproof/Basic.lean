a0b02af1f3487dcf4bfe35627830a51ac5606cb5
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2024-12-09 17:15:35 +0000

    Added VectorSpace assumption

--- Bulletproof/Basic.lean
+++ Bulletproof/Basic.lean
@@ -1,8 +1,16 @@
 /- some mathlib, batteries, aesop imports -/
+import Aesop
+import Mathlib
+import Batteries
 
 /- here goes definition of bulletproof -/
-section definition
+section bulletproofdefinition
 
-  def fn (n : Nat) : Nat := n + 1
+  variable {G : Type*} {F : Type*}
+    [AddCommGroup G] [DecidableEq G]
+    [Field F] [DecidableEq F]
+    [Module F G]
 
-end definition
+
+
+end bulletproofdefinition
